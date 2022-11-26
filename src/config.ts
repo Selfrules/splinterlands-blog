47e3327fe9376910d9442bd392104dea6c30e951
Selfrules <mattia@selfrules.org>
2022-11-26 16:01:01 +0100

    fix: 🐛 meta
    update website indo

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Di cosa parliamo oggi? - Selfrules - Mattia Filippo De Luca';
+export const SITE_DESCRIPTION = 'Qui potete trovare un mix di argomenti della mia vita quotidiana, tra cui Business, Fitness e CriptoGame.';
+export const HOMEPAGE_URL = 'https://www.selfrules.org/'
