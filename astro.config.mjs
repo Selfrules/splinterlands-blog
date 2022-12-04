329bb3e995475e93a3fc40eabe53f3b84a5c6574
Selfrules <mattia@selfrules.org>
2022-12-04 12:54:13 +0100

    feat: 🎸 packege.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,39 +1,42 @@
-import { defineConfig } from 'astro/config'
-import sitemap from '@astrojs/sitemap'
-import react from '@astrojs/react'
-import image from '@astrojs/image'
-import tailwind from '@astrojs/tailwind'
-import addClasses from 'rehype-add-classes'
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import remarkToc from "remark-toc";
+import remarkCollapse from "remark-collapse";
+import sitemap from "@astrojs/sitemap";
+import partytown from '@astrojs/partytown'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://splinterlandsblog.it',
-  base: '/',
+  site: "https://splinterlandsblog.it",
   integrations: [
-    sitemap(),
-    react(),
-    image({
-      serviceEntryPoint: '@astrojs/image/sharp'
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
     }),
-    tailwind()
+    react(),
+    sitemap(),
+    partytown({
+      config: {
+        forward: ['dataLayer.push']
+      }
+    })
   ],
   markdown: {
-    extendDefaultPlugins: true,
-    rehypePlugins: [
+    remarkPlugins: [
+      remarkToc,
       [
-        addClasses,
+        remarkCollapse,
         {
-          h1: 'text-4xl font-bold font-mplus',
-          h2: 'text-2xl font-bold font-mplus',
-          h3: 'text-xl font-bold font-mplus',
-          h4: 'text-lg font-bold font-mplus',
-          h5: 'font-bold font-mplus',
-          h6: 'font-bold font-mplus',
-          img: 'border border-slate-300 dark:border-zinc-700 rounded-xl mb-6',
-          p: 'mb-6',
-          a: 'underline underline-offset-2 hover:text-orange-500 decoration-orange-500'
-        }
-      ]
-    ]
-  }
-})
+          test: "Tabella dei contenuti",
+        },
+      ],
+    ],
+    shikiConfig: {
+      theme: "one-dark-pro",
+      wrap: true,
+    },
+    extendDefaultPlugins: true,
+  },
+});
