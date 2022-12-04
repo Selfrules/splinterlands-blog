8b8a63194b91aa3a9469f7d55d3341dbbaae623d
Selfrules <mattia@selfrules.org>
2022-12-04 12:28:58 +0100

    feat: 🎸 add search page

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,10 +1,28 @@
-import rss from '@astrojs/rss';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../config';
+import { SITE } from "src/config";
+import rss from "@astrojs/rss";
+import type { Frontmatter } from "src/types";
+import type { MarkdownInstance } from "astro";
+import slugify from "@utils/slugify";
+
+const postImportResult = import.meta.glob<MarkdownInstance<Frontmatter>>(
+  "../contents/**/**/*.md",
+  {
+    eager: true,
+  }
+);
+const posts = Object.values(postImportResult);
 
 export const get = () =>
-	rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: import.meta.env.SITE,
-		items: import.meta.glob('./blog/**/*.{md,mdx}'),
-	});
+  rss({
+    title: SITE.title,
+    description: SITE.desc,
+    site: SITE.website,
+    items: posts
+      .filter(({ frontmatter }) => !frontmatter.draft)
+      .map(({ frontmatter }) => ({
+        link: `posts/${slugify(frontmatter)}`,
+        title: frontmatter.title,
+        description: frontmatter.description,
+        pubDate: new Date(frontmatter.datetime),
+      })),
+  });
